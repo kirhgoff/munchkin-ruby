314ee9cbd682fec0dc9aa07ca0826688b64bfc32
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2016-12-20 22:27:29 +1100

    refactored client to use bot thread as well

--- lib/client/mud_client.rb
+++ lib/client/mud_client.rb
@@ -1,35 +1,112 @@
 require 'socket'
+require 'thread'
 
-class MudReader
-  def read_lore(socket)
-    puts "Reader> Started"
-    while true
-      string = socket.recv(4*1024)
-      break if string.nil? || string.empty?
-      print(string)
-      sleep 0.5
+class Looper
+  def initialize(name, delay = 0.1)
+    @stop_flag = false
+    @name = name
+    @delay = delay
+  end
+
+  def loop
+    puts "#{@name}> Started"
+    until @stop_flag
+      puts "#{@name}> Action..."
+      do_action
+      sleep @delay
     end
-    puts "Reader> Finished"
+    puts "#{@name}> Finished"
+
+  end
+
+  def do_action
+    # method to override
+  end
+
+  def stop
+    @stop_flag = true
+    puts "#{@name}> Stopping..."
   end
 end
 
-class MudWriter
-  def read_user_input(socket)
-    puts "Writer> Started"
-    while true
-      line = gets
-      socket.send line + "\n", 0
-    end
-    puts "Writer> Finished"
+class MudReader < Looper
+  def initialize(socket, bot)
+    super("Reader")
+    @socket = socket
+    @bot = bot
   end
+
+  def do_action
+    string = @socket.recv(4*1024)
+    stop if string.nil? || string.empty?
+    print(string) if !string.nil? && !string.empty?
+  end
+end
+
+class UserInput < Looper
+  def initialize(bot)
+    super("UserInput")
+    @bot = bot
+  end
+
+  def do_action
+    @bot.issue_command(gets)
+  end
+end
+
+class DoNothingStrategy
+  def next_command
+    nil
+  end
+end
+
+class Bot < Looper
+  def initialize(socket)
+    super("Bot", 1) #commands are issued once per second
+    @mutex = Mutex.new
+    @strategy = DoNothingStrategy.new
+    @mutex.synchronize do
+      @socket = socket
+    end
+  end
+
+  def do_action
+    command = @strategy.next_command
+    issue_command(command) unless command.nil?
+  end
+
+  def issue_command(command)
+    if command.starts_with? "bot:"
+      print "Bot> Caught bot command #{command}, doing nothing."
+      #TODO parse command
+    else
+      @mutex.synchronize do
+        @socket.send command + "\n", 0
+      end
+    end
+
+  end
+
 end
 
 socket = TCPSocket.new("falloutmud.org", 2222)
-reader = Thread.new {MudReader.new.read_lore(socket)}
-writer = Thread.new {MudWriter.new.read_user_input(socket)}
+bot = Bot.new(socket)
 
-reader.join
-writer.join
+bot_thread = Thread.new do
+  #bot.loop
+end
+
+reader_thread = Thread.new(socket, bot) do |s, b|
+  MudReader.new(s, b).loop
+end
+
+input_thread = Thread.new(bot) do |b|
+  UserInput.new(b).loop
+end
+
+bot_thread.join
+reader_thread.join
+input_thread.join
 
 
 

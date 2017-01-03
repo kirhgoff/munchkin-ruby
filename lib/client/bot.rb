da7ddceddcd945bd0c9fd66cf90bffe52f54aa6f
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2017-01-03 22:49:15 +1100

    login strategy is working

--- lib/client/bot.rb
+++ lib/client/bot.rb
@@ -1,16 +1,16 @@
-class LoreModel
-  attr_accessor :need_login, :need_password
+require 'yaml'
 
-  @need_login = false
-  @need_password = false
-end
+require './lib/client/looper'
 
 class Bot < Looper
+  attr_reader :position
+
   def initialize(socket)
     super("Bot", 1) #commands are issued once per second
     @mutex = Mutex.new
+    @position = Position.new(6, 2)
 
-    @model = LoreModel.new
+    @model = {}
     @strategy = LoginStrategy.new
     # is it needed?
     @mutex.synchronize do
@@ -26,7 +26,7 @@ class Bot < Looper
   def issue_command(command)
     if command.start_with? "bot:"
       print "Bot> Caught bot command #{command}, doing nothing."
-      @strategy = GotoShopStrategy.new if command == "bot:shopping"
+      @strategy = GotoShopStrategy.new(@position) if command == "bot:shopping"
     else
       @mutex.synchronize do
         @socket.send command + "\n", 0
@@ -35,31 +35,72 @@ class Bot < Looper
   end
 
   def parse_lore(string)
-    @model.need_login = string =~ /What do they call ya/
-    @model.need_password = string =~ /Access Code/
+    changes = {}
+    if string =~ /What do they call ya/
+      changes[:login_requested] = true
+    elsif string =~ /Access /
+      changes[:password_requested] = true
+    elsif string =~ /Welcome to/
+      changes[:menu_appeared] = true
+    elsif string =~ /PRESS RETURN/
+      changes[:menu_press_enter] = true
+    end
 
-    @strategy.update(@model)
+    unless changes.empty?
+      puts "Changes: #{changes}"
+      @model.merge!(changes)
+      @strategy.update(changes, @model)
+    end
   end
 end
 
-class LoginStrategy
-  def next_command
-    nil
+class ModelListener
+  def update(changes, model)
   end
 end
 
-
-class DoNothingStrategy
-  def next_command
-    nil
-  end
-end
-
-class GotoShopStrategy
+class LoginStrategy < ModelListener
+  @command = nil
   def initialize
+    @credentials = YAML.load_file("#{ENV['HOME']}/.munchkin")
+    puts "Credentials: #{@credentials}"
+  end
+
+  def next_command
+    result = @command
+    @command = nil
+    puts "\nLogin command=#{result}" unless result.nil?
+    result
+  end
+
+  def update(changes, _)
+    if changes.key?(:password_requested)
+      @command = @credentials[:password]
+    elsif changes.key?(:login_requested)
+      @command = @credentials[:login]
+    elsif changes.key?(:menu_press_enter)
+      @command = ""
+    elsif changes.key?(:menu_appeared)
+      @command = "1"
+    else
+      @command = nil
+    end
+  end
+
+end
+
+
+class DoNothingStrategy  < ModelListener
+  def next_command
+    nil
+  end
+end
+
+class GotoShopStrategy < ModelListener
+  def initialize(position)
     puts "Created goto shop strategy"
     @maze = Maze.new(MAP)
-    @path = @maze.find_path(position(6, 2), position(1, 3)).reverse
+    @path = @maze.find_path(position(6, 2), position).reverse
     puts "path is: #{@path}"
   end
 
